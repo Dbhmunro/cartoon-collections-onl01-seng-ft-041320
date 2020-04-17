76eb29338a88dd0a538d7c3a6244f46f5424d559
David Munro <dbhmunro@gmail.com>
2020-04-17 01:46:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet (elements)# code an argument here
   elements.map { |element| "#{element.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls (call_words)# code an argument here
   # Your code here
+  call_words.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
